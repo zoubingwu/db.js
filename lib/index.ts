839943418cc88989a75d751006f1212a9c5ac3d5
zoubingwu <zoubingwu@gmail.com>
2022-01-20 16:00:16 +0800

    feat: add simplest sql compiler

--- lib/index.ts
+++ lib/index.ts
@@ -1,25 +1,61 @@
 import repl from 'repl';
 
-function evaluate(evalCmd, context, filename, callback) {
-  const cmd = evalCmd.toLowerCase().trim();
-  switch (cmd) {
-    case 'quit':
-    case 'exit': {
-      console.log('Bye!');
-      replServer.close();
-      return;
-    }
-    default:
-      callback(null, `Unrecognized command '${cmd}'.`);
+enum PrepareStatementResult {
+  Success,
+  Unrecognized,
+}
+
+enum StatementType {
+  INSERT,
+  SELECT,
+}
+
+interface Statement {
+  type: StatementType;
+}
+
+function prepareStatement(cmd: string): {
+  stmt?: Statement;
+  result: PrepareStatementResult;
+} {
+  const firstWord = cmd.split(' ')[0].toLowerCase();
+  if (firstWord === 'select') {
+    return {
+      stmt: { type: StatementType.SELECT },
+      result: PrepareStatementResult.Success,
+    };
+  }
+
+  if (firstWord === 'insert') {
+    return {
+      stmt: { type: StatementType.INSERT },
+      result: PrepareStatementResult.Success,
+    };
+  }
+
+  return { result: PrepareStatementResult.Unrecognized };
+}
+
+function executeStatement(stmt: Statement): string {
+  switch (stmt.type) {
+    case StatementType.INSERT:
+      return 'This is where we would do an insert.\n';
+    case StatementType.SELECT:
+      return 'This is where we would do a select.\n';
   }
 }
 
-function modifyOutput(output: string) {
-  return output.trim();
-}
-
-const replServer = repl.start({
+repl.start({
   prompt: 'db.js >> ',
-  eval: evaluate,
-  writer: modifyOutput,
+  eval: (evalCmd: string, _, __, callback) => {
+    const cmd = evalCmd.trim();
+    const { stmt, result } = prepareStatement(cmd);
+
+    if (result === PrepareStatementResult.Unrecognized) {
+      callback(null, `Unrecognized command '${cmd}'.`);
+    } else {
+      callback(null, executeStatement(stmt!));
+    }
+  },
+  writer: output => output.trim(),
 });
