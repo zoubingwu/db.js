c3608979348676b8464de2a29f4f344062702c5f
zoubingwu <zoubingwu@gmail.com>
2022-01-24 19:21:46 +0800

    chore: use ts-node to run program

--- db.ts
+++ db.ts
@@ -2,6 +2,7 @@ import repl from 'repl';
 
 enum PrepareStatementResult {
   Success,
+  SyntaxError,
   Unrecognized,
 }
 
@@ -12,29 +13,38 @@ enum StatementType {
 
 interface Statement {
   type: StatementType;
+  cmd: string;
 }
+
+// type Row = [number, string, string];
 
 function prepareStatement(cmd: string): {
   stmt?: Statement;
   result: PrepareStatementResult;
 } {
   const firstWord = cmd.split(' ')[0].toLowerCase();
-  if (firstWord === 'select') {
+
+  if (firstWord === 'insert') {
+    if (!isLegalInsertStatement(cmd)) {
+      return { result: PrepareStatementResult.SyntaxError };
+    }
     return {
-      stmt: { type: StatementType.SELECT },
+      stmt: { type: StatementType.INSERT, cmd },
       result: PrepareStatementResult.Success,
     };
   }
 
-  if (firstWord === 'insert') {
+  if (cmd === 'select') {
     return {
-      stmt: { type: StatementType.INSERT },
+      stmt: { type: StatementType.SELECT, cmd },
       result: PrepareStatementResult.Success,
     };
   }
 
   return { result: PrepareStatementResult.Unrecognized };
 }
+
+const InsertStatementRegex = /^insert\s+([1-9]\d*)\s+(\w+)\s+([a-z@.]+)$/i;
 
 function executeStatement(stmt: Statement): string {
   switch (stmt.type) {
@@ -45,14 +55,20 @@ function executeStatement(stmt: Statement): string {
   }
 }
 
+function isLegalInsertStatement(cmd: string) {
+  return InsertStatementRegex.test(cmd);
+}
+
 repl.start({
   prompt: 'db.js >> ',
-  eval: (evalCmd, _, __, callback) => {
+  eval: (evalCmd, context, filename, callback) => {
     const cmd = evalCmd.trim();
     const { stmt, result } = prepareStatement(cmd);
 
     if (result === PrepareStatementResult.Unrecognized) {
-      callback(null, `Unrecognized command '${cmd}'.`);
+      callback(null, `Unrecognized keyword at start of '${cmd}'.\n`);
+    } else if (result === PrepareStatementResult.SyntaxError) {
+      callback(null, `Syntax error. Could not parse statement.\n`);
     } else {
       callback(null, executeStatement(stmt!));
     }
